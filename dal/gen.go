827c3ed7224d673324e3139484932b91ff9d55f8
macbook-air <techcom.ec20@gmail.com>
2022-03-28 17:58:22 +0700

    aa

---
+++ dal/gen.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fiber-crud/pkg/config"
+
+	"gorm.io/gen"
+)
+
+func init() {
+	config.InitialConfig()
+	config.InitialDB()
+}
+func main() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "./dal/query",
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery,
+		/* Mode: gen.WithoutContext|gen.WithDefaultQuery*/
+		//if you want the nullable field generation property to be pointer type, set FieldNullable true
+		/* FieldNullable: true,*/
+		//if you want to assign field which has default value in `Create` API, set FieldCoverable true, reference: https://gorm.io/docs/create.html#Default-Values
+		/* FieldCoverable: true,*/
+		//if you want to generate index tags from database, set FieldWithIndexTag true
+		/* FieldWithIndexTag: true,*/
+		//if you want to generate type tags from database, set FieldWithTypeTag true
+		FieldWithTypeTag: true,
+		//if you need unit tests for query code, set WithUnitTest true
+		/* WithUnitTest: true, */
+	})
+	// db, _ := gorm.Open(sqlserver.Open("sqlserver://sa:p@ssw0rd@Localhost?database=GolangDemo&charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(config.Db())
+	g.ApplyBasic(g.GenerateAllTable()...)
+	g.Execute()
+}
--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/shopspring/decimal"
+	"gorm.io/gen"
 )
 
 func init() {
 	config.InitialConfig()
-	// config.InitialDB()
+	config.InitialDB()
 }
 
 func main() {
@@ -24,7 +25,30 @@ func main() {
 
 	// Middleware
 	app.Use(middleware.Logger)
-	app.Use(middleware.Authorization())
+	// app.Use(middleware.Authorization())
+	genSQL()
+
+	// q := query.Use(config.Db())
+	// address := q.Address
+	// user := q.User
+	// u, _ := user.Preload(address.AddressID).Find()
+	// fmt.Println(u)
+
+	// a, _ := q.Address.Preload(uu.AddressID).Find()
+	// fmt.Println(a)
+
+	// lat, err := decimal.NewFromString(string(u.Lat))
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// long, err := decimal.NewFromString(string(u.Long))
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(lat.Add(long))
+	// var tt []uint8
+
+	// tt = decimal.NewFromString("")
 
 	// Routes
 	routes.PublicRoutes(app)
@@ -36,4 +60,26 @@ func main() {
 	} else {
 		utils.StartServerWithGracefulShutdown(app)
 	}
+}
+
+func genSQL() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "./dal/query",
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery,
+		/* Mode: gen.WithoutContext|gen.WithDefaultQuery*/
+		//if you want the nullable field generation property to be pointer type, set FieldNullable true
+		/* FieldNullable: true,*/
+		//if you want to assign field which has default value in `Create` API, set FieldCoverable true, reference: https://gorm.io/docs/create.html#Default-Values
+		/* FieldCoverable: true,*/
+		//if you want to generate index tags from database, set FieldWithIndexTag true
+		/* FieldWithIndexTag: true,*/
+		//if you want to generate type tags from database, set FieldWithTypeTag true
+		FieldWithTypeTag: true,
+		//if you need unit tests for query code, set WithUnitTest true
+		/* WithUnitTest: true, */
+	})
+	// db, _ := gorm.Open(sqlserver.Open("sqlserver://sa:p@ssw0rd@Localhost?database=GolangDemo&charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(config.Db())
+	g.ApplyBasic(g.GenerateAllTable()...)
+	g.Execute()
 }
