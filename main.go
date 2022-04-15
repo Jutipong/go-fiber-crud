e27c82715497ae6bdb2348b749dcc264d441e745
Jutipong.Dev <techcom.ec20@gmail.com>
2022-04-15 23:04:15 +0700

    remove comment

--- main.go
+++ main.go
@@ -27,9 +27,7 @@ func main() {
 	decimal.MarshalJSONWithoutQuotes = true
 	app := fiber.New(fiber.Config{DisableStartupMessage: true})
 	app.Use(cors.New(cors.Config{AllowOrigins: "*", AllowMethods: "*", AllowHeaders: "*"}))
-	//test 1
-	//test 2
-	//test 3
+
 	// Middleware
 	app.Use(middleware.Logger)
 	// app.Use(middleware.Authorization())
