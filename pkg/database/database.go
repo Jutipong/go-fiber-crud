e16a46026bdecd5d4c1ef61a805484efab9b3b01
jutipong.su <techcom.ec20@gmail.com>
2022-11-17 21:31:39 +0700

    update code

---
+++ pkg/database/database.go
@@ -0,0 +1,76 @@
+package database
+
+import (
+	"database/sql"
+	"fiber-crud/pkg/config"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type User struct {
+	ID           uint
+	Name         string
+	Email        *string
+	Age          uint8
+	Birthday     *time.Time
+	MemberNumber sql.NullString
+	ActivatedAt  sql.NullTime
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	TT           string
+}
+
+var _db *gorm.DB
+
+func InitialDB() {
+	configDb := getConfigDb()
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,         // Disable color
+		},
+	)
+	db, err := gorm.Open(sqlserver.Open(configDb+"&charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{Logger: newLogger})
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	_db = db
+	//Auto Migrate
+	// db.Table("Userx").AutoMigrate(&User{})
+}
+
+func getConfigDb() string {
+	conf := config.Database()
+	if conf.Port > 0 {
+		return fmt.Sprintf(
+			"sqlserver://%s:%s@%s:%d?database=%s",
+			conf.User,
+			conf.Pass,
+			conf.Server,
+			conf.Port,
+			conf.DatabaseName,
+		)
+	} else {
+		return fmt.Sprintf(
+			"sqlserver://%s:%s@%s?database=%s",
+			conf.User,
+			conf.Pass,
+			conf.Server,
+			conf.DatabaseName,
+		)
+	}
+}
+
+func Db() *gorm.DB {
+	return _db
+}
