4be32d8d42eb22c4c2ba4b9aac79e446a0e8f1f0
jutipong.su <techcom.ec20@gmail.com>
2022-12-04 17:29:22 +0700

    update code

--- main.go
+++ main.go
@@ -20,21 +20,25 @@ func main() {
 	db := database.Db()
 
 	start := time.Now()
-	err := db.CreateInBatches(&data, 100).Error
+	err := db.CreateInBatches(&data, 300).Error
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	end := time.Since(start)
 
-	fmt.Println(fmt.Sprintf("insert data: %v record time: %v", len(data), end))
+	// var elapsedTime = String.Format("insert data: {0} record time: {1:00}:{2:00}:{3:00}.{4:00}", customers.Count(),
+	// ts.Hours, ts.Minutes, ts.Seconds,
+	// ts.Milliseconds / 10);
+
+	fmt.Println(fmt.Sprintf("insert data: %v record time: %.2f:%.2f:%v", len(data), end.Hours(), end.Minutes(), end.Milliseconds()))
 
 }
 
 func MockData() []database.Customer {
-	start := time.Now()
+	// start := time.Now()
 	result := []database.Customer{}
-	for i := 0; i < 1000000; i++ {
+	for i := 0; i < 500000; i++ {
 		result = append(result, database.Customer{
 			Id:          uuid.NewString(),
 			Name:        fmt.Sprintf("Name: %v", i),
@@ -45,8 +49,8 @@ func MockData() []database.Customer {
 		})
 	}
 
-	end := time.Since(start)
-	fmt.Println(fmt.Sprintf("mock data: %v", end))
+	// end := time.Since(start)
+	// fmt.Println(fmt.Sprintf("mock data: %v", end))
 
 	return result
 }
